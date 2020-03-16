b794584492ab4c31c681360b406493fd29677971
QuartoTR <tristan.michel@etu.umpc.fr>
2020-03-16 22:45:00 +0100

    Mise en place de l'architecture des fichiers

---
+++ functions.asm
@@ -0,0 +1,180 @@
+# ---------------------------------------------------------------------------
+# FUNCTIONS SEGMENT
+#
+# Text segment (function code) for the program.
+#
+#
+#	Rules :
+#
+# The caller is responsible for saving and restoring any of the following
+# caller-saved registers that it cares about.
+# 	$t0-$t9		$a0-$a3		$v0-$v1
+#
+# The callee is responsible for saving and restoring any of the following
+# callee-saved registers that it uses. (Remember that $ra is “used” by jal.)
+#	$s0-$s7		$ra
+#
+# Registers $a0–$a3 (4–7) are used to pass the first four arguments to routines
+# (remaining arguments are passed on the stack). Registers $v0 and $v1
+# (2,3) are used to return values from functions.
+#
+# ---------------------------------------------------------------------------
+
+	.text
+	.align 2
+
+# ---------------------------------------------------------------------------
+# Public 	(.globl)
+# ---------------------------------------------------------------------------
+
+
+	.globl init_plateau
+init_plateau:
+
+	# Remplir le plateau de pieces pour le début de partie
+
+	la $t0, plateau
+	li $t1, 0 	# indice deplacement dans plateau
+	li $t4, 4
+	li $t5, 36 	# nb cases plateau
+init_plateau_FOR:
+	sll $t2, $t1, 1 	# *2 pour half
+	add $t2, $t2, $t0 	# adresse de la nouvelle case
+	div $t1, $t4 	# indice mod 4
+	mfhi $t3
+	srl $t3, $t3, 1		# /2
+	addi $t3, $t3, 1	# +1
+	sh $t3, 0($t2)
+	addi $t1, $t1, 1 	# +1 indice
+	blt $t1, $t5, init_plateau_FOR		# si num case < 36 => reboucle
+init_plateau_END_FOR:
+	jr $ra
+
+#--------------------#
+
+
+	.globl print_game_state
+print_game_state:		# NULL
+
+	# Affiche l'etat complet du jeu
+
+	jr $ra
+
+#--------------------#
+
+
+	.globl ask_player_cell
+ask_player_cell:		# num du joueur 
+
+	# Renvoie la coordonnée x de la case valide selectionnée
+	# Renvoie la coordonnée y de la case valide selectionnée
+
+	jr $ra
+
+#--------------------#
+
+
+	.globl ask_player_nb_pieces
+ask_player_nb_pieces:		# coord case x, coord case y
+
+	# Renvoie 0 si le joueur pose des pieces ou 1 si il en deplace
+	# Renvoie le nombre de pieces que le joueur actuel veut deplacer ou alors poser
+
+	jr $ra
+
+#--------------------#
+
+
+	.globl ask_player_direction
+ask_player_direction:		# coord case x, coord case y, nb pieces
+
+	# Renvoie la direction voulu pour deplacer la pile de pieces du joueur actuel
+
+	jr $ra
+
+#--------------------#
+
+
+	.globl move_piece
+move_piece:			# NULL
+
+	# 
+
+	jr $ra
+
+
+# ---------------------------------------------------------------------------
+# Private
+# ---------------------------------------------------------------------------
+
+
+print_new_line:			# NULL
+
+	# Affiche un saut de ligne dans la console
+
+nl: .asciiz "\n"
+	la $a0, nl
+	li $v0, 4
+	syscall
+	jr $ra
+
+#--------------------#
+
+
+print_plateau:			# NULL
+
+	# Affiche le plateau de jeu avec les pieces qu'il contient ainsi que la reserve
+
+	jr $ra
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+#PRINT: j'aime trop kenza, kenza ma vie, love you kenza, kenza c'est la meilleure, je suis trop centent de la connaitre, c'est trop cool, Kenza est cool, MIPS c'est pas cool haha haha haha vive kenza, kenza est vrmt la meilleure, elle ecoute pas les rageux du genre 'AMINATA' LOL, waw kenza est trop belle je l'adore ! Je suis Tristqn et je suis nul, aller hop c'est cadeau
--- main.asm
+++ main.asm
@@ -10,28 +10,23 @@
 plateau: .space 72	# stockage : un half pour une case soit 16 bits pour 5 pions => pion sur 2 bit [01] 1 (+) / [10] 2 (*) / [00] vide
 reserve: .byte 0, 0
 
-phrase_choix_case_depart_0_x: .asciiz "Indiquer la coordonnée x de départ pour le joueur 1 (+) : "
-phrase_choix_case_depart_0_y: .asciiz "Indiquer la coordonnée y de départ pour le joueur 1 (+) : "
-phrase_choix_case_depart_1_x: .asciiz "Indiquer la coordonnée x de départ pour le joueur 2 (*) : "
-phrase_choix_case_depart_1_y: .asciiz "Indiquer la coordonnée y de départ pour le joueur 2 (*) : "
-phrase_choix_case_arrive_0_x: .asciiz "Indiquer la coordonnée x d'arrivé pour le joueur 1 (+) : "
-phrase_choix_case_arrive_0_y: .asciiz "Indiquer la coordonnée y d'arrivé pour le joueur 1 (+) : "
-phrase_choix_case_arrive_1_x: .asciiz "Indiquer la coordonnée x d'arrivé pour le joueur 2 (*) : "
-phrase_choix_case_arrive_1_y: .asciiz "Indiquer la coordonnée y d'arrivé pour le joueur 2 (*) : "
+noms_joueurs: .asciiz "joueur 1 (+)", "joueur 2 (*)"
+phrase_choix_case_1: .asciiz "Indiquer la coordonnée "
+phrase_choix_case_2: .asciiz " de la case de depart pour le "
+coord_x_y: .asciiz "x", "y"
+
 phrase_nb_piece_deplacement: .asciiz "Combien de pieces voulez vous déplacer ? "
-phrase_victoire_1: .asciiz "Felicitation qu joueur 1 (+) qui a gagné !"
-phrase_victoire_2: .asciiz "Felicitation qu joueur 2 (*) qui a gagné !"
+phrase_victoire_1: .asciiz "Felicitation au "
+phrase_victoire_2: .asciiz " qui a gagné !"
 
-affichage_pions: .asciiz  "   ", "[+]", "[*]"		# 4 char ascii par string : 4 octets pour décallage
-affichage_grille: .asciiz "|", "+---------------+---------------+---------------+---------------+---------------+---------------+"	# 2 ascii : 2 octets décallage
-
-affichage_saut_ligne: .asciiz "\n"
+affichage_case: .asciiz  "   ", "[+]", "[*]", "-->", "<--"	# 4 char ascii par string : 4 octets pour décallage
+affichage_grille: .asciiz "|", "+------------------+------------------+------------------+------------------+------------------+------------------+"	# 2 ascii : 2 octets décallage
 
 
 # ---------------------------------------------------------------------------
 # MAIN SEGMENT
 #
-# Text segment (code) for the program.
+# Text segment (main code) for the program.
 # ---------------------------------------------------------------------------
 
 	.text
@@ -46,114 +41,3 @@ main:
 	
 	li $v0, 10
 	syscall		# Instruction de fin de programme
-
-
-# ---------------------------------------------------------------------------
-# FUNCTIONS SEGMENT
-#
-# Text segment (code) for the program.
-#
-#
-#	Rules :
-#
-# The caller is responsible for saving and restoring any of the following
-# caller-saved registers that it cares about.
-# 	$t0-$t9		$a0-$a3		$v0-$v1
-#
-# The callee is responsible for saving and restoring any of the following
-# callee-saved registers that it uses. (Remember that $ra is “used” by jal.)
-#	$s0-$s7		$ra
-#
-# Registers $a0–$a3 (4–7) are used to pass the first four arguments to routines
-# (remaining arguments are passed on the stack). Registers $v0 and $v1
-# (2,3) are used to return values from functions.
-#
-# ---------------------------------------------------------------------------
-
-	.text
-	.align 2
-
-init_plateau:
-
-	# Remplir le plateau de pieces pour le début de partie
-
-	la $t0, plateau
-	li $t1, 0 	# indice deplacement dans plateau
-	li $t4, 4
-	li $t5, 36 	# nb cases plateau
-init_plateau_FOR:
-	sll $t2, $t1, 1 	# *2 pour half
-	add $t2, $t2, $t0 	# adresse de la nouvelle case
-	div $t3, $t1, $t4 	# indice mod 4
-	mfhi $t3
-	srl $t3, $t3, 1		# /2
-	addi $t3, $t3, 1	# +1
-	sh $t3, 0($t2)
-	addi $t1, $t1, 1
-	blt $t1, $t5, init_plateau_FOR
-init_plateau_END_FOR:
-	jr $ra
-
-#--------------------#
-
-print_new_line:
-
-	# Faire un saut de ligne dans la console
-
-	la $a0, affichage_saut_ligne
-	li $v0, 4
-	syscall
-	jr $ra
-
-#--------------------#
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-
-#PRINT: j'aime trop kenza, kenza ma vie, love you kenza, kenza c'est la meilleure, je suis trop centent de la connaitre, c'est trop cool, Kenza est cool, MIPS c'est pas cool haha haha haha vive kenza, kenza est vrmt la meilleure, elle ecoute pas les rageux du genre 'AMINATA' LOL, waw kenza est trop belle je l'adore ! Je suis Tristqn et je suis nul, aller hop c'est cadeau
